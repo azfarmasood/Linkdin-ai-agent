f7cf114cd75abbdf356d7d9c4207a767f2ce8f3c
azfarmasood <azfarmasood22@gmail.com>
2025-01-21 18:56:24 +0500

    project_updated

--- langchain_linkdin_search_agent/app.py
+++ langchain_linkdin_search_agent/app.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from langchain_linkdin_search_agent.linkdin_agent import linkedin_search_agent
+from langchain_linkdin_search_agent.models.summary_model import Agent_Response
 
 app: FastAPI = FastAPI()
+
+chat_history: list[Agent_Response] = []
+
 
 @app.get("/")
 def checking_route():
     return {"message": "Hello Linkedin AI Agent."}
 
-@app.post("/search-profile/")
+@app.get("/get-linkdin-data/", response_model=list[Agent_Response])
+async def get_linkdin_profile_data():
+    return JSONResponse(content=chat_history)
+
+@app.post("/search-profile/", response_model=Agent_Response)
 async def linkedin_profile_agent(name: str):
     result = linkedin_search_agent(name)
 
@@ -16,10 +24,11 @@ async def linkedin_profile_agent(name: str):
         raise HTTPException(status_code=404, detail="Profile not found.")
 
     summary, profile_pic_url = result
+    
+    agent_response = Agent_Response(summary=summary.to_dict(), profile_pic_url=profile_pic_url)
+    
+    chat_history.append(agent_response.model_dump())
 
     return JSONResponse(
-        content={
-            "summary_and_facts": summary.to_dict(),
-            "profile_pic_url": profile_pic_url,
-        }
+        content=[agent_response.model_dump()]
     )
--- langchain_linkdin_search_agent/models/summary_model.py
+++ langchain_linkdin_search_agent/models/summary_model.py
@@ -1,5 +1,7 @@
+from typing import Any
 from pydantic import BaseModel, Field
 from langchain_core.output_parsers import PydanticOutputParser
+
 
 class Summary(BaseModel):
     summary: str = Field(description = "A Short Summary About Linkedin Profile")
@@ -7,5 +9,12 @@ class Summary(BaseModel):
     
     def to_dict(self):
         return {"summary": self.summary, "facts": self.facts}
+
+
+class Agent_Response(BaseModel):
+    summary: Summary = Field(description="Summary and facts about the LinkedIn profile")
+    profile_pic_url: str = Field(description = "URL of the Profile Picture")
+    
+    
     
 summary = PydanticOutputParser(pydantic_object = Summary)
